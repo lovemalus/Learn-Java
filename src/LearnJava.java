5c0878ebf02430a039bfb12a40961456d13a6a5f
phanzo <luvmalus@gmail.com>
2020-03-30 11:47:22 +0800

    Hello World!

--- src/LearnJava.java
+++ src/LearnJava.java
@@ -1,6 +1,17 @@
+/**
+* This program is input Hello World!
+*
+*/
+
 public class LearnJava {
 
-    public static void main(String[] args) {
-	      System.out.println("learn java");
+    public static void main(String[] args)
+    {
+        String greeting = "Hello World!";
+        System.out.println(greeting);
+        // 打印出和 greeting一样长度的“=”
+        for (int i = 0; i < greeting.length();i++)
+            System.out.print("=");
+        System.out.println();
     }
 }
