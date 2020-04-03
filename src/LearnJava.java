f60346641ceabc4a749b9b3e92f9b9d016a90807
phanzo <luvmalus@gmail.com>
2020-04-03 21:43:53 +0800

    update LearnJava&create Retirement

--- src/LearnJava.java
+++ src/LearnJava.java
@@ -8,8 +8,11 @@ public class LearnJava {
     public static void main(String[] args)
     {
         String greeting = "Hello World!";
+        //join add delimiter /
+        String all = String.join("/","S","M","L","XL");
         System.out.println(greeting);
-        // 打印出和 greeting一样长度的“=”
+        System.out.println(all);
+        // Print the same "=" as "greeting"
         for (int i = 0; i < greeting.length();i++)
             System.out.print("=");
         System.out.println();
