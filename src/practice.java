a32d858992b4956be876e6b68b19b9d35c963304
phanzo <luvmalus@gmail.com>
2020-04-10 19:49:34 +0800

    A Blastoff gadget.

---
+++ src/practice.java
@@ -0,0 +1,20 @@
+/**
+ * This is a practice for some little tools.
+ *
+ * @author: Phanzo
+ * @create: 2020-04-05 21:38
+ */
+public class practice {
+
+    public static void main(String[] args){
+        Blastoff();
+    }
+
+    //This is a Blastoff gadget.
+    public static void Blastoff(){
+        for (int i = 10; i > 0; i--)
+            System.out.println("Count down ... " + i);
+        System.out.println("Blastoff!");
+    }
+
+}
